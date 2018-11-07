3ae9023952f2440aab654f5b21360df792f107bc
George Dunk <gdunkgd@gmail.com>
2018-11-07 12:05:23 +0000

    Blank java UI

---
+++ MainWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class MainWindow {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Movie DB");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.pack();
+
+		frame.setVisible(true);
+
+	}
+
+}
